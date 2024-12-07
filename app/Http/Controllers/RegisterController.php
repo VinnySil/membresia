3ad6ecc3489ada0c13ef60434eb38e2494957659
Vinicius <vinicius.silvestrefr@gmail.com>
2024-12-07 21:57:46 +0100

    registro de clientes

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -10,8 +12,22 @@ class RegisterController extends Controller
 
         //validamos los datos del usuario que nos llega
         $request->validate([
-            
+            'full_name' => 'required|max:100',
+            'nick' => 'required|unique:users|max:50',
+            'nif' => 'required|unique:users|min:9|max:9',
+            'email' => 'required|unique:users|max:255',
+            'password' => 'required|max:255',
+            'born_date' => 'required|date'
         ]);
+
+        $datos = $request->all(); //saco todos los datos que llegan del frontend
+        $datos['rol_id'] = 3; //añado el rol de cliente al usuario
+        $user = User::create($datos);
+
+        //Autenticamos el usuario
+        Auth::login($user);
+
+        return redirect(route('home'));
 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/register', function(){
+    return view('auth.register');
+})->name('register');
+
+Route::post('/register', [RegisterController::class, 'register'])->name('post.register');
 
 //Endpoint para el crud de usuarios
 Route::resource('users', UserController::class);
