3302baf884a0396b5202437a4c2933af212e3a67
Vinicius <vinicius.silvestrefr@gmail.com>
2024-12-11 19:49:21 +0100

    rutas organizadas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\auto\PermissionController;
+use App\Http\Controllers\auto\RolController;
 use App\Http\Controllers\users\AccountController;
 use App\Http\Controllers\users\LoginController;
 use App\Http\Controllers\users\RegisterController;
@@ -7,36 +9,32 @@ use App\Http\Controllers\users\UserController;
 use App\Http\Middleware\CheckRol;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+//Rutas base
+Route::get('/', function () {return view('welcome');})->name('home');
+Route::get('/no-autorizado', function () {return view('no_autorizado');})->name('no-autorizado');
 
-Route::get('/no-autorizado', function () {
-    return view('no_autorizado');
-})->name('no-autorizado');
-
-Route::get('/register', function(){
-    return view('auth.register');
-})->name('register');
-
-Route::get('/login', function(){
-    return view('auth.login');
-})->name('login');
-
+//Rutas para el login y registro de usuarios
+Route::get('/register', function(){return view('auth.register');})->name('register');
+Route::get('/login', function(){return view('auth.login');})->name('login');
 Route::post('/register', [RegisterController::class, 'register'])->name('post.register');
 Route::post('/login', [LoginController::class, 'login'])->name('post.login');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function(){
-    //Endpoint para el crud de usuarios
-    Route::resource('users', UserController::class)->middleware(CheckRol::class.':Administrador,Editor');
-    
+
+    Route::middleware(CheckRol::class.':Administrador,Editor')->group(function(){
+        //Endpoint para el crud de usuarios
+        Route::resource('users', UserController::class);
+        //Endpoint para el crud de permisos
+        Route::resource('permissions', PermissionController::class);
+        //Endpoint para el crud de roles
+        Route::resource('rols', RolController::class);
+    });
     //Endpoints para las cuentas de usuarios
     Route::controller(AccountController::class)->group(function(){
         Route::get('/account/{user}', 'show')->name('account.show');
         Route::patch('/account/{user}', 'update')->name('account.update');
         Route::delete('/account/{user}', 'destroy')->name('account.destroy');
     });
-    
 });
 
