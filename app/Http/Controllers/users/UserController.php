a332283cabc4d213e19acf2589c29cd8779be5ac
Vinicius <vinicius.silvestrefr@gmail.com>
2024-12-20 14:42:05 +0100

    definir un rol al crear un usuario crud

--- app/Http/Controllers/users/UserController.php
+++ app/Http/Controllers/users/UserController.php
@@ -23,7 +23,8 @@ class UserController extends Controller
     */
    public function create()
    {
-       return view('users.create');
+       $rols = Rol::all();
+       return view('users.create', compact('rols'));
    }
 
     /**
